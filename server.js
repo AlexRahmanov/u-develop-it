3130979d061bcb1a2d65e43b109c00eb4085402b
Alexander <alexr722@gmail.com>
2022-05-05 13:54:38 -0400

    cnadidates update

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+require("dotenv").config();
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: process.env.USER,
+      // Your MySQL password
+      password: process.env.PASSWORD,
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
